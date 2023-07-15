33a69f2e7ab75c2d027b2f2548400f2b22deee33
Nizz <nizarbonex1@gmail.com>
2023-07-15 12:57:53 +0200

    sistemato

--- cmd_invite.cpp
+++ cmd_invite.cpp
@@ -14,50 +14,38 @@ void	Client::invite(t_server &srv)
 	std::string cmd, cmd_nickname, chan;
 	std::getline(iss, cmd, ' ');
 	std::getline(iss, cmd_nickname, ' ');
-	std::getline(iss, chan, '\n'); //TODO: if another clinet command might end with another command the just \n
+	std::getline(iss, chan, '\n'); //TODO: if another client command might end with another command the just \n
 	if (chan[0] == '#')
 		chan.erase(chan.begin());
 
-	//TODO: check if the command is well formatted
 	if (cmd_nickname.empty() || chan.empty())
 	{
-		//TODO: check if you need to disconnect the client or send a msg
+		std::string reply = "\033[31mERROR :Command is not formated correctly!\033[0m\r\n";
+		send(_fd, reply.c_str(), reply.length(), 0);
 		return ;
 	}
-
-	//TODO: check if the client sending the command is in the channel
 	if (!srv.channels[chan].isInChannel(srv.nicknames[this->nickname]))
 	{
-		std::string reply = "ERR_NOTONCHANNEL (442) :you are not even in the channel\r\n";
+		std::string reply = "\033[31mERR_NOTONCHANNEL (442) :you are not even in the channel\033[0m\r\n";
 		send(_fd, reply.c_str(), reply.length(), 0);
-		//TODO : check if you need to disconnect
 		return ;
 	}
-	
-	//TODO: check if the client is an operator in the channel
 	if (this->isOpInChannel(chan, srv) == false)
 	{
 		std::string reply = "ERR_CHANOPRIVSNEEDED (482) :You're not channel operator\r\n";
 		send(_fd, reply.c_str(), reply.length(), 0);
-		//TODO : check if you need to disconnect
 		return ;
 	}
-
-	//TODO: check if the channel exists
 	if (srv.channels.find(chan) == srv.channels.end())
 	{
 		std::string reply = "ERR_NOSUCHCHANNEL (403) :No such channel\r\n";
 		send(_fd, reply.c_str(), reply.length(), 0);
-		//TODO : check if you need to disconnect
 		return ;
 	}
-
-	//TODO: check if the client is in the channel already
 	if (srv.channels[chan].isInChannel(srv.nicknames[cmd_nickname]))
 	{
 		std::string reply = "ERR_USERONCHANNEL (443) :the client is already in the channel\r\n";
 		send(_fd, reply.c_str(), reply.length(), 0);
-		//TODO : check if you need to disconnect
 		return ;
 	}
 
